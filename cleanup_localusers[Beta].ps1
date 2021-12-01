3a425c1ea2cc924874b17a290429ab7a80daadba
Sergio Ruiz <62002026+Grsergio7@users.noreply.github.com>
2021-12-01 15:03:56 -0700

    Helps declutter computer
    We have some old machines with too many profiles and that slows down the computer and leaves no internal storage. This is still in testing as there are only a few of those old machines. It has worked but I am not fully confident it will ALWAYS work as expected since I always go in and make some changes

---
+++ cleanup_localusers[Beta].ps1
@@ -0,0 +1,6 @@
+﻿New-PSSession -ComputerName AZ000550
+cd C:\Users
+$who = Get-ChildItem -Path 'C:\Users' -exclude Admin,Public,ad.sergio.ruiz,zi.cho
+Remove-Item $who -recurse -force
+
+#$filter =  where-Object { $_.LastWriteTime -lt (Get-Date "1/1/21") -notlike '.\Admin,Public,ad.sergio.ruiz,zi.cho'}
